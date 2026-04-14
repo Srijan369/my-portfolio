e8ed378f19704ab8e6326b08a2d83feb8f1da653
Srijan369 <yadavsrijan18@gmail.com>
2026-04-14 21:09:51 +0530

    Add runtime.txt and fix build

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# Exit on error
-set -o errexit
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Run migrations
-python manage.py migrate
-
-# Collect static files
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
